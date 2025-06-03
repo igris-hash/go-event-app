43628f87bd78e6c41023ca679af5257ef7542f4e
vikas <vikaskumar.e1256@gmail.com>
2025-06-03 15:57:59 +0530

    feat: delete event api added

--- routes/event.go
+++ routes/event.go
@@ -76,3 +76,22 @@ func updateEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully"})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	event, err := models.GetEventById(eventID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not delete event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully"})
+}
