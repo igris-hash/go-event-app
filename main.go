09a0bf6af227a15ed16b4bce52917daf443558f7
vikas <vikaskumar.e1256@gmail.com>
2025-06-03 12:53:44 +0530

    chore(deps): installed air for hot reload and setting up a first route

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import(
+import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
-func main() {
 
+func main() {
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.Run(":8081")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello world"})
 }
